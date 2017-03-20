24742e007ddcf77610e32f15d61f799661c27c7d
Ty Le <anhtuan.n.le@gmail.com>
2017-03-20 17:25:10 -0400

    Add opacity to search bar

--- search-link.js
+++ search-link.js
@@ -1,1 +1,14 @@
+$(document).ready(function() {
+
+// $("form input").click(function() {
+// 	$(this).parent().addClass("search-underline").siblings().removeClass("search-underline")
+		
+// 		})
+
+
+		$("body").click(function(){
+	    $("input[type=text]").addClass("search-underline");
+	});
+
+});
 
