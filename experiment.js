85555731b5159336b90d45077c06d53adc223a11
AgathaLynn <amaskelton@verizon.net>
2017-03-25 01:59:21 -0400

    combined todo.js and focus-todo.js

--- experiment.js
+++ experiment.js
@@ -3,28 +3,46 @@ $(document).ready(function() {
   // main focus
   var mainFocus = Object.create(List);
   mainFocus.setup("focus-list", "#focus-list");
-  $(mainFocus.target).on("click", "li span", mainFocus.handleModifications);
 
-  $("#focus-query").submit(mainFocus.handleSubmit);
+  $(mainFocus.target).on("click", "li span", function() {
+    var span = this;
+    mainFocus.handleModifications(span);
+  });
+
+  $("#focus-query").submit(function(evt) {
+    evt.preventDefault();
+    if (mainFocus.list.length === 0) {
+      mainFocus.addItem($("#focus-input").val());
+    }
+    $("#focus-input").val("");
+  });
 
   // todo list
   var toDos = Object.create(List);
   toDos.setup("todo-list", "#todo-list");
-  $(toDos.target).on("click", "li span", toDos.handleModifications);
 
-  $("#newItem").submit(toDos.handleSubmit);
+  $(toDos.target).on("click", "li span", function() {
+    var span = this;
+    toDos.handleModifications(span);
+  });
+
+  $("#newItem").submit(function(evt) {
+    evt.preventDefault();
+    toDos.addItem($("#todo-input").val());
+    $("#todo-input").val("");
+  });
 
   $(".expand-todo").on("click", function() {
     $(".todoContainer").toggle();
   });
-  
+
 });
 
 var List = {
   setup: function(loc, target) {
     this.loc = loc;
     this.target = target;
-    this.list = getList();
+    this.list = this.getList();
     this.updateDisplay();
   },
   getList: function() {
@@ -32,26 +50,15 @@ var List = {
     return lst ? JSON.parse(lst) : [];
   },
   addItem: function(task_name) {
-    if (task_name.length === 0) {
-      return;
-    }
-    var obj = {};
-    obj.name = task_name;
-    obj.complete = false;
+    if (task_name.length > 0) {
+      var obj = {};
+      obj.name = task_name;
+      obj.complete = false;
 
-    this.list.push(obj);
-    this.saveList();
-    this.updateDisplay();
-  },
-  toggleComplete: function(index) {
-    this.list[index].complete = !this.list[index].complete;
-    this.saveList();
-    this.updateDisplay();
-  },
-  deleteItem: function(index) {
-    this.list.splice(index, 1);
-    this.saveList();
-    this.updateDisplay();
+      this.list.push(obj);
+      localStorage.setItem(this.loc, JSON.stringify(this.list));
+      this.updateDisplay();
+    }
   },
   updateDisplay: function() {
     var todos = "", className;
@@ -65,23 +72,15 @@ var List = {
     }
     $(this.target).html(todos);
   },
-  saveList: function() {
+  handleModifications: function(span) {
+    var index = $(span).parent().index();
+    if ($(span).hasClass("checkBox")) {
+      this.list[index].complete = !this.list[index].complete;
+    }
+    else if ($(span).hasClass("delete")) {
+      this.list.splice(index, 1);
+    }
     localStorage.setItem(this.loc, JSON.stringify(this.list));
-  },
-  handleSubmit: function(evt, obj) {
-    event.preventDefault();
-    obj.addItem($("input").val());
-    $("input").val("");
-  },
-  handleModifications: function(evt) {
-    // Note: "this" refers to clicked span
-    var index = $(this).parent().index();
-
-    if ($(this).hasClass("checkBox")) {
-      toggleComplete(index);
-    }
-    else if ($(this).hasClass("deleteTodo")) {
-      deleteItem(index);
-    }
+    this.updateDisplay();
   }
 };
