ec603585d57679eec0445b5b8caa7b4b4e8ab508
AgathaLynn <amaskelton@verizon.net>
2017-04-05 20:10:29 -0400

    removed 'training wheels' - css outlines, fake quote, etc.

--- quote.js
+++ quote.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
   var qod = $.getJSON("http://quotes.rest/qod.json?category=inspire", function(data) {
     console.log(data);
-    $("#quotes").html(/*data.contents.quotes[0].quote*/ "That's my last duchess painted on the wall / looking as if she were alive. I call / that piece a wonder now. Fra Pandolf's hand / worked busily a day and there she stands. Will it please you sit and look at her? I said...");
+    $("#quotes").html(data.contents.quotes[0].quote);
     $("#quote-author").html(data.contents.quotes[0].author);
   });
 
