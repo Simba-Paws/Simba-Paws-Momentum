74b15c090553adeaba0970bf464be6afe20bb84f
AgathaLynn <amaskelton@verizon.net>
2017-04-05 20:05:52 -0400

    fixed quote positioning

--- quote.js
+++ quote.js
@@ -1,13 +1,13 @@
 $(document).ready(function() {
   var qod = $.getJSON("http://quotes.rest/qod.json?category=inspire", function(data) {
     console.log(data);
-    $("#quotes").html(data.contents.quotes[0].quote);
+    $("#quotes").html(/*data.contents.quotes[0].quote*/ "That's my last duchess painted on the wall / looking as if she were alive. I call / that piece a wonder now. Fra Pandolf's hand / worked busily a day and there she stands. Will it please you sit and look at her? I said...");
     $("#quote-author").html(data.contents.quotes[0].author);
   });
 
 $("#quote-container").hover(function() {
 	$("#quote-citation").toggle();
-  $("#quote-container").css("max-height", "92px");
+  $("#quote-container").toggleClass("onhover");
 });
 
 });
